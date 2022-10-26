17fff79d4d3907046ccba9d79908b0bbb72d1091
ssaid <94416699+swywssaid@users.noreply.github.com>
2022-10-26 16:15:32 +0900

    feat: Solve 1evel0 나이 출력
    - use Date object and getFullYear method to get current year

---
+++ algorithm/algorithm-problems/programmers/level0/나이 출력/better.js
@@ -0,0 +1,9 @@
+function solution(age) {
+  const stdYear = new Date().getFullYear();
+  var answer = stdYear - age + 1;
+  return answer;
+}
+
+/*
+1. 기준년도를 Date 객체를 통해 현재 년도를 설정해줌.
+*/
---
+++ algorithm/algorithm-problems/programmers/level0/나이 출력/mine.js
@@ -0,0 +1,10 @@
+function solution(age) {
+  const stdYear = 2022;
+  var answer = stdYear - age + 1;
+  return answer;
+}
+
+/* 
+1. 기준 년도의 수정을 대비해 변수로 설정
+
+*/

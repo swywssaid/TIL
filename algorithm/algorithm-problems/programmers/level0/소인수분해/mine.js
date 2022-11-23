f3cccd450d7568f3c436b9d7360a271961e23ad2
ssaid <94416699+swywssaid@users.noreply.github.com>
2022-11-23 23:50:41 +0900

    feat: Solve level0 소인수분해

--- algorithm/algorithm-problems/programmers/level0/소인수분해/mine.js
+++ algorithm/algorithm-problems/programmers/level0/소인수분해/mine.js
@@ -1,4 +1,14 @@
-function solution(n) {
-  var answer = [];
-  return answer;
+function getPrimeFactor(n) {
+  let answer = [];
+  let divider = 2;
+  while (n != 1) {
+    if (n % divider === 0) {
+      answer.push(divider);
+      n /= divider;
+    } else {
+      divider++;
+    }
+  }
+  return console.log([...new Set(answer)]);
 }
+getPrimeFactor(10000);

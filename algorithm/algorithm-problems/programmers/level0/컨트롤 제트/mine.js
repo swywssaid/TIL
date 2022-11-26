66d1e00a77afd20a81e1a2b09c8689c0e4a4db69
ssaid <94416699+swywssaid@users.noreply.github.com>
2022-11-27 00:48:10 +0900

    fix: Update level0 컨트롤 제트

--- algorithm/algorithm-problems/programmers/level0/컨트롤 제트/mine.js
+++ algorithm/algorithm-problems/programmers/level0/컨트롤 제트/mine.js
@@ -1,8 +1,16 @@
 function solution(s) {
   let answer = 0;
-  s.split(" ").forEach((item, index, arr) => {
-    answer += item !== "Z" ? Number(item) : -Number(arr[index - 1]) ? -Number(arr[index - 1]) : 0;
+  let sumList = [];
+  s.split(" ").forEach((item, index) => {
+    if (item !== "Z") {
+      sumList.push(Number(item));
+      answer += Number(item);
+    } else {
+      if (sumList[sumList.length - 1]) {
+        answer -= sumList.pop();
+      } else sumList.push(0);
+    }
   });
-  return console.log(answer);
+  return answer;
 }
-solution("Z 10 Z Z 20 Z 1 Z");
+solution("Z 10 Z Z 20 30 Z Z 40");

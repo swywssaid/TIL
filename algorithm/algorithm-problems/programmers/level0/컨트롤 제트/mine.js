63bacc7abfe68eddcf67f219d65c278408e8760a
ssaid <94416699+swywssaid@users.noreply.github.com>
2022-11-26 23:58:07 +0900

    fix: Update level0 컨트롤 제트

--- algorithm/algorithm-problems/programmers/level0/컨트롤 제트/mine.js
+++ algorithm/algorithm-problems/programmers/level0/컨트롤 제트/mine.js
@@ -3,5 +3,6 @@ function solution(s) {
   s.split(" ").forEach((item, index, arr) => {
     answer += item !== "Z" ? Number(item) : -Number(arr[index - 1]) ? -Number(arr[index - 1]) : 0;
   });
-  return answer;
+  return console.log(answer);
 }
+solution("Z 10 Z Z 20 Z 1 Z");

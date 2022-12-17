152a68ced8014a38d0296ac97687ce5006f4a628
ssaid <94416699+swywssaid@users.noreply.github.com>
2022-12-17 18:46:28 +0900

    feat: Solve level0 종이 자르기

---
+++ algorithm/algorithm-problems/programmers/level0/종이 자르기/better.js
@@ -0,0 +1,7 @@
+function solution(M, N) {
+  return M * N - 1;
+}
+
+/*
+이것도 내풀이와 같은 것...
+*/
---
+++ algorithm/algorithm-problems/programmers/level0/종이 자르기/mine.js
@@ -0,0 +1,8 @@
+function solution(M, N) {
+  return M * N !== 1 ? M * N - 1 : 0;
+}
+
+/*
+better.js가 내 코드 리팩토링한것...
+조건 중복됨. 
+*/

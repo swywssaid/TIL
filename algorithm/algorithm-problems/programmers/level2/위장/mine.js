08e269a55eade03aa10370fd8a67af025a62ba1e
ssaid <94416699+swywssaid@users.noreply.github.com>
2022-12-25 23:59:22 +0900

    feat: Being solved level2 위장

--- algorithm/algorithm-problems/programmers/level2/위장/mine.js
+++ algorithm/algorithm-problems/programmers/level2/위장/mine.js
@@ -1,11 +1,26 @@
 function solution(clothes) {
   let map = new Map();
+  const fac = (arr) => {
+    arr.map((el) => {
+      let leng = 0;
+      el.forEach((_, i) => (leng *= i + 1));
+      return leng;
+    });
+    arr.redue();
+  };
+
   clothes.forEach((c) => {
-    map.set(c[1], c[0]);
+    map.set(c[1], map.get(c[1]) ? [...map.get(c[1]), c[0]] : [c[0]]);
   });
+
   console.log(map);
   return;
 }
+let map = new Map();
+map.set("a", [1, 2]);
+console.log(map.get("a"));
 /*
-
-*/
+1 -> clothes.length
+2~key 종류 -> 4개 같은 것을 포함한 뽑기
+총개수C뽑는개수/중복!
+ */

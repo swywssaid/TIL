24293754a68679dac7b87845ce90e1208b2f5524
ssaid <94416699+swywssaid@users.noreply.github.com>
2022-12-26 22:04:46 +0900

    fix: Being solved level1 위장

--- algorithm/algorithm-problems/programmers/level2/위장/mine.js
+++ algorithm/algorithm-problems/programmers/level2/위장/mine.js
@@ -1,26 +1,56 @@
 function solution(clothes) {
   let map = new Map();
-  const fac = (arr) => {
-    arr.map((el) => {
-      let leng = 0;
-      el.forEach((_, i) => (leng *= i + 1));
-      return leng;
-    });
-    arr.redue();
-  };
 
   clothes.forEach((c) => {
     map.set(c[1], map.get(c[1]) ? [...map.get(c[1]), c[0]] : [c[0]]);
   });
 
-  console.log(map);
-  return;
+  return [...map.keys()]
+    .map((_, i) => {
+      if (i === 0) return comb(clothes.length, i + 1);
+      return comb(clothes.length, i + 1);
+    })
+    .reduce((acc, cur) => acc + cur, 0);
 }
+
+solution([
+  ["yellow_hat", "headgear"],
+  ["blue_sunglasses", "eyewear"],
+  ["green_turban", "headgear"],
+]);
 let map = new Map();
 map.set("a", [1, 2]);
-console.log(map.get("a"));
+// console.log([...map.keys()].length);
+// console.log(
+//   Array(4)
+//     .fill(0)
+//     .map((x) => console.log(x))
+// );
 /*
 1 -> clothes.length
 2~key 종류 -> 4개 같은 것을 포함한 뽑기
-총개수C뽑는개수/중복!
+총개수C뽑는개수 * 해당 품목의 개수* ...
+[3,2,2,1]
+4C2 3*2 + 3*2 + 3*1 + 2*2 + 2*1 + 2*1  = 23
+4C3 3*2*2 3*2*1 3*2*1 2*2*1 = 
+4C4 3*2*2*1
+const fac = (arr) => {
+  return arr
+    .map((el) => {
+      let leng = 1;
+      el.forEach((_, i) => (leng *= i + 1));
+      return leng;
+    })
+    .reduce((acc, cur) => acc * cur, 1);
+};
+
+const comb = (n, a) => {
+  a = a <= n - a ? a : n - a;
+  let mul = 1;
+  for (let i = n; i > n - a; i--) {
+    mul *= i;
+    console.log(i, mul);
+  }
+  return mul / fac([new Array(a).fill(0)]);
+};
  */
